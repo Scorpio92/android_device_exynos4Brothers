a002f95ced62ff6ed57c4c6737c419cec931917b
Scorpio92 <scorpio92@scorpio92-desktop.(none)>
2013-06-29 20:52:33 +1100

    поправил дату

--- cm.mk
+++ cm.mk
@@ -64,7 +64,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy 
+    ro.com.android.dateformat=dd-MM-yyyy 
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
